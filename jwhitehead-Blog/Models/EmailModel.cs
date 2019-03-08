0fe8641c9c5255cd4b6a2fc181c1a80f523b5bba
juddw <juddwhitehead@gmail.com>
2019-03-07 19:09:03 -0500

    Added email field to test spam live.
    Will show extra field to test.

--- jwhitehead-Blog/Models/EmailModel.cs
+++ jwhitehead-Blog/Models/EmailModel.cs
@@ -3,14 +3,16 @@
 namespace jwhitehead_Blog.Models
 {
     public class EmailModel
-        {
-            [Required, Display(Name = "Name")]
-            public string FromName { get; set; }
-            [Required, Display(Name = "Email"), EmailAddress]  // login view model
-            public string FromEmail { get; set; }
-            [Required] // if hard coding the subject in the HomeController, remove this line.
-            public string Subject { get; set; }
-            [Required]
-            public string Body { get; set; }
-        }
+    {
+        [Required, Display(Name = "Name")]
+        public string FromName { get; set; }
+        [Required, Display(Name = "Email"), EmailAddress]  // login view model
+        public string FromEmail { get; set; }
+        [Display(Name = "Email2"), EmailAddress]  // login view model, block spammers! Hidden field.
+        public string FromEmail2 { get; set; }
+        [Required] // if hard coding the subject in the HomeController, remove this line.
+        public string Subject { get; set; }
+        [Required]
+        public string Body { get; set; }
+    }
 }
--- jwhitehead-Blog/obj/Release/AspnetCompileMerge/Source/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="jwhitehead_Blog.MvcApplication" Language="C#" %>
